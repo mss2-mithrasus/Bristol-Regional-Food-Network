c47eb16fc3e72d109d4664ba0ee5ea4d08e50553
l2-karim <Lano2.Karim@live.uwe.ac.uk>
2026-02-17 11:34:59 +0000

    testing

--- product_management/models.py
+++ product_management/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from user.models import User
+from product_catalog.models import Category
+
 
 # Create your models here.
+
+class Product(models.Model):
+    # define atttibutes
+    producer = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    unit = models.CharField(max_length=50)
+    stock_quantity = models.IntegerField()
+    harvest_data = models.DateField()
+    image = models.ImageField(upload_to='products/')
+    
+    
