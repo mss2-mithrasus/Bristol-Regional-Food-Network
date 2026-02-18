4915a4aed4516b01d13146ac929c4dc49471be86
m2-palha <Micaiah2.Palha@live.uwe.ac.uk>
2026-02-18 16:45:59 +0000

    ID-53 micaiah created producers models.py

--- BRFN/urls.py
+++ BRFN/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from producers.views import db_test
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('db-test/', db_test),
 ]
--- producers/views.py
+++ producers/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.db import connection
 
-# Create your views here.
+def db_test(request):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SHOW TABLES;")
+            tables = cursor.fetchall()
+        return HttpResponse(f"Database OK — Tables: {tables}")
+    except Exception as e:
+        return HttpResponse(f"Database ERROR: {e}")
