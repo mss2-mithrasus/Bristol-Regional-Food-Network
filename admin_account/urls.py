61341671f3920eb313d6e098c804fc1d3f2f4ddb
m2-palha <Micaiah2.Palha@live.uwe.ac.uk>
2026-03-12 12:50:57 +0000

    ID-70 fixed registration, login and admin login and delete

--- admin_account/urls.py
+++ admin_account/urls.py
@@ -3,7 +3,10 @@ from .views import (
     AdminHomePageView,
     PendingAccountsView,
     ApproveAccountView,
-    RejectAccountView
+    RejectAccountView,
+    # ADDED (10-03-26)
+    DeletedAccountsView,
+    FailedLoginAttemptsView
 )
 
 urlpatterns = [
@@ -11,4 +14,8 @@ urlpatterns = [
     path("pending/", PendingAccountsView.as_view(), name="admin_pending"),
     path("approve/", ApproveAccountView.as_view(), name="admin_approve"),
     path("reject/", RejectAccountView.as_view(), name="admin_reject"),
+    # ADDED (10-03-26)
+    path("deleted-accounts/", DeletedAccountsView.as_view(), name="admin_deleted_accounts"),
+    path("failed-logins/", FailedLoginAttemptsView.as_view(), name="admin_failed_logins"),
+
 ]
