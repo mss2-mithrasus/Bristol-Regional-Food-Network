0e01703097a2560214f2ad9863c9c29840f19809
Felna D'silva <felna2.dsilva@live.uwe.ac.uk>
2026-03-13 12:51:50 +0000

    ID-74-felna-changes-weekly payments

--- BRFN/urls.py
+++ BRFN/urls.py
@@ -1,21 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
-from user_accounts.views import home_page, registration_page, login_page
+from user_accounts.views import home_page,  registration_page, login_page
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+
+
 
 urlpatterns = [
-    path('customer/home/', include("product.urls")),
+    #path("", home_page, name="home"),
+    # path("login/", login_page, name="login"),
+    # path("register/", registration_page, name='register'),
+    path('', include("product.urls")),
     path("", include("user_accounts.urls")),
-    path("producer/", include("producers.urls")),
-    path("admin-dashboard/", include("admin_account.urls")),
-    path('cart/', include('shopping_cart.urls')), 
+    
+    
     path('admin/', admin.site.urls),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
-    path("orders/", include("order_management.urls")), 
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
+    #path('db-test/', db_test),
+    path('', include('product.urls')),
+    path("producer/", include("producers.urls")),
+    #path('db-test/', db_test),
+    #path('', include('product.urls'))
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
